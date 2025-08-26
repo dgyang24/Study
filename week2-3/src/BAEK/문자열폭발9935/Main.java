d6f711d0874ddbe27eea64a3d77b6c46d333f7c9
were2019 <were2019@naver.com>
2025-08-26 16:05:42 +0900

    week2-3

--- week2-3/src/BAEK/문자열폭발9935/Main.java
+++ week2-3/src/BAEK/문자열폭발9935/Main.java
@@ -1,9 +1,45 @@
 package BAEK.문자열폭발9935;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		String M = sc.next();
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			stack.push(c);
+			StringBuilder tmp = new StringBuilder();
+			for (int j = M.length() - 1; j >= 0; j--) {
+				if(stack.isEmpty()||stack.peek() != M.charAt(j)) {
+					//if(j !=  M.length() - 1 ) {
+						tmp.reverse();
+						for(int k = 0; k < tmp.length(); k++) {
+							stack.push(tmp.charAt(k));
+						}
+					//}
+					break;
+				}
+				if(!stack.isEmpty()) {
+					tmp.append(stack.pop());
+				}
+				
+				
+			}
+		}
+		if(stack.size() == 0) {
+			System.out.println("FRULA");
+		}else {
+			StringBuilder sb = new StringBuilder();
+			while(!stack.empty()) {
+				sb.append(stack.pop());
+			}
+			System.out.println(sb.reverse());
+		}
 
 	}
 
---
+++ week2-3/src/BAEK/프린터큐1966/Main.java
@@ -0,0 +1,26 @@
+package BAEK.프린터큐1966;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			Queue<Integer> q = new LinkedList<>();
+			for(int i = 0; i < N; i++) {
+				q.add(sc.nextInt());
+				
+			}
+			
+			
+		}
+
+	}
+
+}
