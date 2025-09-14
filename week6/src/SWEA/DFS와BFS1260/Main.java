07726337e8ca540d790b667cff0f2fbe2bb8b86a
were2019 <were2019@naver.com>
2025-09-15 00:17:17 +0900

    hw

---
+++ week6/src/SWEA/DFS와BFS1260/Main.java
@@ -0,0 +1,86 @@
+package SWEA.DFS와BFS1260;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static List<Integer>[] adj;
+	static boolean[] visited;
+	static StringBuilder sbD;
+	static StringBuilder sbB;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		int M = sc.nextInt();
+		int V = sc.nextInt();
+		visited = new boolean[N + 1];
+		sbD = new StringBuilder();
+		sbB = new StringBuilder();
+		adj = new ArrayList[N + 1];
+		for (int i = 1; i <= N; i++)
+			adj[i] = new ArrayList<>();
+		// 인접리스트에 간선 정보입력
+		for (int i = 0; i < M; i++) {
+			int from = sc.nextInt();
+			int to = sc.nextInt();
+			//양방향
+			adj[from].add(to);
+			adj[to].add(from);
+		}
+		//정렬
+		for(int i = 1; i < adj.length; i++) {
+			Collections.sort(adj[i]);
+		}
+		// dfs, bfs 각 실행하고
+		dfs(V);
+		visited = new boolean[N + 1];
+		bfs(V);
+		// 출력
+		System.out.println(sbD);
+		System.out.println(sbB);
+
+	}
+
+	static void dfs(int v) {
+		visited[v] = true;
+		sbD.append(v).append(" ");
+		for (int i = 0; i < adj[v].size(); i++) {
+			int n =adj[v].get(i);
+			if (visited[n]) continue;
+			dfs(n);
+		}
+	}
+
+	static void bfs(int v) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(v);
+		while (!q.isEmpty()) {
+			int n = q.poll();
+			if (visited[n])
+				continue;
+			visited[n] = true;
+			sbB.append(n).append(" ");
+			for (int i = 0; i < adj[n].size(); i++) {
+				q.add(adj[n].get(i));
+			}
+
+		}
+
+	}
+
+	static boolean isTrue() {
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i])
+				return false;
+		}
+		return true;
+	}
+
+}
---
+++ week6/src/SWEA/미로탐색2178/Main.java
@@ -0,0 +1,64 @@
+package SWEA.미로탐색2178;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+	
+	static int N;
+	static int M;
+	static char[][] maze;
+	static int[][] visited;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		visited = new int[N][M];
+		maze = new char[N][M];
+		for(int i = 0; i < N; i++) {
+			String str = sc.next();
+			for(int j = 0; j < M; j++) {
+				maze[i][j] = str.charAt(j);
+			}
+//			System.out.println(Arrays.toString(maze[i]));
+		}
+		bfs(0,0);
+		
+		System.out.println(visited[N-1][M-1]); 
+		
+	}
+	static void bfs(int r, int c) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {r,c});
+		visited[r][c] = 1;
+		
+		while(!q.isEmpty()) {
+			int[] curr = q.poll();
+			int cr = curr[0];
+			int cc = curr[1];
+			
+			for(int i = 0; i < 4; i++) {
+				int nr = cr + dr[i];
+				int nc = cc + dc[i];
+			
+				if(nr < 0 || nc < 0 || nr >= N || nc >= M) continue;
+				if(maze[nr][nc] == '1' && visited[nr][nc] == 0) {
+					visited[nr][nc] = visited[cr][cc]+1;
+					q.add(new int[] {nr,nc});
+				}
+			}
+			
+			
+			
+		}
+		
+	}
+	
+	
+
+}
