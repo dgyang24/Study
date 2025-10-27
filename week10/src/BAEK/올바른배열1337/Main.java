24e4b9b704c2bbd3e793e1be833b8b04f0dcfa61
were2019 <were2019@naver.com>
2025-10-27 22:09:17 +0900

    week10

--- week10/src/BAEK/올바른배열1337/Main.java
+++ week10/src/BAEK/올바른배열1337/Main.java
@@ -1,9 +1,32 @@
 package BAEK.올바른배열1337;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		int ans = Integer.MAX_VALUE;
+		for(int i = 0; i < N; i++)
+			arr[i] = sc.nextInt();
+		Arrays.sort(arr);
+		for(int i = 0; i < N; i++) {
+			int cnt = 0;
+			for(int j = i; j < j+5; j++){
+				if(j >= N) break;
+				if(arr[j] < arr[i]+5)
+					cnt++;
+			}
+			cnt = 5-cnt;
+			ans = Math.min(ans, cnt);
+			if(ans == 0) {
+				break;
+			}
+		}
+		System.out.println(ans);
 
 	}
 
